4b995d0c04f7f1b7e35e645fa4263902a4d337e5
samhurley14 <samhurley141414@gmail.com>
2023-08-11 12:11:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,4 @@ streamlit.header("The fruit loda list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.text('Thanks for adding ',add_my_fruit)
+streamlit.write('Thanks for adding ',add_my_fruit)
